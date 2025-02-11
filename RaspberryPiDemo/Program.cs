a1678b1cd2d55d76e83954fc4c55b2665e7f1ef2
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-02-11 18:02:42 +0500

    Added code to demo

--- RaspberryPiDemo/Program.cs
+++ RaspberryPiDemo/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Device.Gpio;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+var pin = 18;
+
+using var controller = new GpioController();
+var openedPin = controller.OpenPin(pin, PinMode.Output);
+var ledOn = true;
+while (true)
+{
+    openedPin.Write(((ledOn) ? PinValue.High : PinValue.Low));
+    // controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
+    await Task.Delay(1000);
+    ledOn = !ledOn;
+}
